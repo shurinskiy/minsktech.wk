40a5fb50572f675d35be0ea22c654fe50f83b668
Alexandr Tumanov <shurinskiy2@gmail.com>
2024-11-17 17:05:06 +0500

    add reviews to main page

---
+++ src/blocks/brands/brands.js
@@ -0,0 +1,25 @@
+import Swiper from 'swiper';
+import { Navigation, Mousewheel } from 'swiper/modules';
+
+(() => {
+
+	new Swiper('.brands__items.swiper', {
+		modules: [Navigation, Mousewheel],
+		slidesPerView: 'auto',
+		// mousewheel: true,
+		navigation: {
+			prevEl: `.brands__button_prev`,
+			nextEl: `.brands__button_next`,
+			disabledClass: 'disabled'
+		},
+		breakpoints: {
+			0: {
+				spaceBetween: 14,
+			},
+			780: {
+				spaceBetween: 20,
+			}
+		}
+	})	
+
+})();
--- src/js/blocks.js
+++ src/js/blocks.js
@@ -3,3 +3,5 @@ import "../blocks/search/search.js";
 import "../blocks/hero/hero.js";
 import "../blocks/categories/categories.js";
 import "../blocks/slider/slider.js";
+import "../blocks/brands/brands.js";
+import "../blocks/rating/rating.js";
